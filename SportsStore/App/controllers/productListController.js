b2daaac063861427736048e4ee29799fdf499c66
Igor Minkin <igor.minkin@gmail.com>
2014-04-25 06:48:02 -0400

    paging

--- SportsStore/App/controllers/productListController.js
+++ SportsStore/App/controllers/productListController.js
@@ -1,11 +1,21 @@
 ﻿angular.module("sportsStore")
     .constant("productListActiveClass", "btn-primary")
-    .controller("productListCtrl", function($scope, $filter, productListActiveClass) {
+    .constant("productListPageCount", 3)
+    .controller("productListCtrl", function ($scope, $filter,
+        productListActiveClass, productListPageCount) {
         var selectedCategory = null;
+
+        $scope.selectedPage = 1;
+        $scope.pageSize = productListPageCount;
 
         $scope.selectCategory = function(newCategory) {
             selectedCategory = newCategory;
+            $scope.selectedPage = 1;
         };
+
+        $scope.selectPage = function(newPage) {
+            $scope.selectedPage = newPage;
+        }
 
         $scope.categoryFilterFn = function(product) {
             return selectedCategory == null || product.category == selectedCategory;
@@ -13,5 +23,9 @@
 
         $scope.getCategoryClass = function(category) {
             return selectedCategory == category ? productListActiveClass : "";
+        }
+
+        $scope.getPageClass = function(page) {
+            return $scope.selectedPage == page ? productListActiveClass : "";
         }
 });
